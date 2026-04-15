7a8b7752ca345cb230e795bc2c92e59713f5d40f
shifat97 <shifatbinreza2@gmail.com>
2026-04-15 20:51:11 +0600

    move test function inside class according to its type

--- tests/test_get_teachers.py
+++ tests/test_get_teachers.py
@@ -1,75 +1,70 @@
 from api.teachers_api import get_teacher
 
 
-# Positive test
+class TestGetTeachers:
+    # Testing endpoint to get all teachers
+    def test_get_teachers_status_code(self, base_url, auth_header, teacher_payload):
+        response = get_teacher(base_url=base_url, auth_header=auth_header)
 
-# Testing endpoint to get all teachers
-def test_get_teachers_status_code(base_url, auth_header, teacher_payload):
-    response = get_teacher(base_url=base_url, auth_header=auth_header)
+        # Validate status code
+        assert response.status_code == 200, f"Expected 200, Got {response.status_code}"
 
-    # Validate status code
-    assert response.status_code == 200, f"Expected 200, Got {response.status_code}"
+    # Validate payload structure
+    def test_get_teachers_payload_structure(self, base_url, auth_header, test_payload_structure):
+        response = get_teacher(base_url=base_url, auth_header=auth_header)
 
+        assert response.status_code == 200, f"Expected 200, Got {response.status_code}"
+        teachers = response.json()
 
-# Validate payload structure
-def test_get_teachers_payload_structure(base_url, auth_header, test_payload_structure):
-    response = get_teacher(base_url=base_url, auth_header=auth_header)
+        for teacher in teachers:
+            for key, expected_type in test_payload_structure.items():
+                key_type = type(teacher.get(key))
 
-    assert response.status_code == 200, f"Expected 200, Got {response.status_code}"
-    teachers = response.json()
+                # Validate if key in missing or not in the response
+                assert key in teacher, f"{key} is missing on teacher: {teacher}"
+                # Validate key type
+                assert key_type == expected_type, f"Expected {expected_type}, Got {key_type}"
+                # Validate if any value is null or not
+                assert teacher[key] is not None, f"{teacher['_id']} on {key} has null value: {teacher[key]}"
 
-    for teacher in teachers:
-        for key, expected_type in test_payload_structure.items():
-            key_type = type(teacher.get(key))
+    # Validate if duplicate id exists or not
+    def test_get_teachers_duplicate_id(self, base_url, auth_header):
+        response = get_teacher(base_url=base_url, auth_header=auth_header)
 
-            # Validate if key in missing or not in the response
-            assert key in teacher, f"{key} is missing on teacher: {teacher}"
-            # Validate key type
-            assert key_type == expected_type, f"Expected {expected_type}, Got {key_type}"
-            # Validate if any value is null or not
-            assert teacher[key] is not None, f"{teacher['_id']} on {key} has null value: {teacher[key]}"
+        assert response.status_code == 200, f"Expected 200, Got {response.status_code}"
+        teachers = response.json()
 
+        ids = [teacher["teacherId"] for teacher in teachers]
+        assert len(ids) == len(set(ids)), "Duplicate id exists in the list"
 
-# Validate if duplicate id exists or not
-def test_get_teachers_duplicate_id(base_url, auth_header):
-    response = get_teacher(base_url=base_url, auth_header=auth_header)
+    # Validate if created teacher is in the list or not
+    def test_get_teacher_after_creation(self, base_url, auth_header, created_teacher):
+        response = get_teacher(base_url=base_url, auth_header=auth_header)
 
-    assert response.status_code == 200, f"Expected 200, Got {response.status_code}"
-    teachers = response.json()
+        assert response.status_code == 200, f"Expected 200, Got {response.status_code}"
+        teachers = response.json()
 
-    ids = [teacher["teacherId"] for teacher in teachers]
-    assert len(ids) == len(set(ids)), "Duplicate id exists in the list"
-
-
-# Validate if created teacher is in the list or not
-def test_get_teacher_after_creation(base_url, auth_header, created_teacher):
-    response = get_teacher(base_url=base_url, auth_header=auth_header)
-
-    assert response.status_code == 200, f"Expected 200, Got {response.status_code}"
-    teachers = response.json()
-
-    ids = [teacher["teacherId"] for teacher in teachers]
-    teacher_id = created_teacher["teacherId"]
-    assert teacher_id in ids, f"{teacher_id} is not in the list after creation"
+        ids = [teacher["teacherId"] for teacher in teachers]
+        teacher_id = created_teacher["teacherId"]
+        assert teacher_id in ids, f"{teacher_id} is not in the list after creation"
 
 
-# Negative test
+class TestGetTeacherAuthorization:
+    # Testing get teachers without authorization header
+    def test_get_teacher_without_authorization(self, base_url):
+        response = get_teacher(base_url=base_url, auth_header={})
 
-# Testing get teachers without authorization header
-def test_get_teacher_without_authorization(base_url):
-    response = get_teacher(base_url=base_url, auth_header={})
-
-    # Validate status code
-    assert response.status_code == 401, f"Expected 401, Got {response.status_code}"
-    # Validate message
-    assert response.json()["message"] == "Missing or invalid Authorization header", "Message is incorrect"
+        # Validate status code
+        assert response.status_code == 401, f"Expected 401, Got {response.status_code}"
+        # Validate message
+        assert response.json()["message"] == "Missing or invalid Authorization header", "Message is incorrect"
 
 
-# Testing get teachers filter with invalid token
-def test_get_teacher_with_invalid_token(base_url, auth_header, auth_header_with_invalid_token):
-    response = get_teacher(base_url=base_url, auth_header=auth_header_with_invalid_token)
+    # Testing get teachers filter with invalid token
+    def test_get_teacher_with_invalid_token(self, base_url, auth_header, auth_header_with_invalid_token):
+        response = get_teacher(base_url=base_url, auth_header=auth_header_with_invalid_token)
 
-    # Validate status code
-    assert response.status_code == 401, f"Expected 401, Got {response.status_code}"
-    # Validate message
-    assert "Invalid" in response.json().get("message", "")
+        # Validate status code
+        assert response.status_code == 401, f"Expected 401, Got {response.status_code}"
+        # Validate message
+        assert "Invalid" in response.json().get("message", "")
